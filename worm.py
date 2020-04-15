bae45fe874d65259e65d667734003ea7d95dee5e
Pandede <steven9609@gmail.com>
2020-04-15 16:12:19 +0800

    Add comments

--- worm.py
+++ worm.py
@@ -18,23 +18,29 @@ class Worm:
         self.password = getpass('Password of FACEBOOK: ')
     
     def __login(self):
+        # Locate the textbox and enter the correspondings
         self.driver.find_element_by_id("email").send_keys(self.username)
         self.driver.find_element_by_id("pass").send_keys(self.password)
         self.driver.find_element_by_id("loginbutton").click()
+        
+        # Wait the respond of website
         time.sleep(1)
     
     def __scroll_down(self, times):
+        # Scroll the browser about k-times
         for i in range(times):
             self.driver.execute_script('window.scrollTo(0, %d)' % (500 * (i+1)))
             time.sleep(1)
     
     def __open_feed(self, feed_id):
+        # Once the keyword matches, open the feed on a blank page
         feed_link = self.group_link + 'permalink/' + feed_id
         self.driver.execute_script('window.open("%s","_blank");' % feed_link)
         self.driver.switch_to.window(self.driver.window_handles[0])
         
     def start(self, group_link):
-        # Open browser and go to FACEBOOK
+        # Opens browser and go to FACEBOOK
+        # Requires 'chromedriver.exe'
         profile = webdriver.ChromeOptions()
         prefs = {'profile.default_content_setting_values.notifications': 2}
         profile.add_experimental_option('prefs', prefs)
@@ -49,13 +55,14 @@ class Worm:
         self.driver.get(group_link + '?sorting_setting=CHRONOLOGICAL')
     
     def listen(self, keyword, top=10, scroll_times=6, freq=30):
-        self.seen = []
+        # Keep listening the feeds and refresh after (freq) secs
+        self.seen = []      # Record the seen feed
         print('[Start] %s' % str(datetime.today()))
         print('------------------------------')
         while True:
             self.__scroll_down(scroll_times)
-            message_list = self.get_messages(top)
-            id_list = self.get_feed_ids(top)
+            message_list = self.__get_messages(top)
+            id_list = self.__get_feed_ids(top)
             matched = list(map(lambda m: m.find(keyword) != -1, message_list))
             match_id = list(compress(id_list, matched))
             
@@ -64,6 +71,7 @@ class Worm:
             print('Matched ID:', match_id)
             print('------------------------------')
             
+            # Open the feed if it hasn't seen before.
             for feed_id in match_id:
                 if feed_id not in self.seen:
                     self.seen.append(feed_id)
@@ -72,7 +80,8 @@ class Worm:
             time.sleep(freq)
             self.driver.refresh()
     
-    def get_feed_ids(self, top):
+    def __get_feed_ids(self, top):
+        # Capture the ID of feed
         subtitle_block = self.driver.find_elements_by_xpath("//div[@data-testid='story-subtitle']")
         feed_list = []
         for i, block in enumerate(subtitle_block[:top]):
@@ -81,7 +90,8 @@ class Worm:
             if m: feed_list.append(m.group(1))
         return feed_list
     
-    def get_messages(self, top):
+    def __get_messages(self, top):
+        # Capture all paragraphs of feed
         p_block = self.driver.find_elements_by_xpath("//div[@data-testid='post_message']")
         message_list = []
         for i, block in enumerate(p_block[:top]):
@@ -98,4 +108,5 @@ class Worm:
         return message_list
 
     def stop(self):
+        # Shut down the driver
         self.driver.quit()
